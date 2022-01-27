708ad1b32c779bae18f71048d0e012c3a1a85623
fjh <1834879843@qq.com>
2022-01-27 16:56:09 +0800

    6-解析body

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,19 @@
 /*
  * @Author: your name
  * @Date: 2022-01-27 15:51:36
- * @LastEditTime: 2022-01-27 15:52:18
+ * @LastEditTime: 2022-01-27 16:47:54
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \api\src\app\index.js
  */
 const Koa = require('koa');
+const KoaBody = require('koa-body');
 
 const userRouter = require('../router/user.route');
 
 const app = new Koa()
 
+app.use(KoaBody())
 app.use(userRouter.routes())
 
 module.exports = app
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,16 +1,26 @@
 /*
  * @Author: your name
  * @Date: 2022-01-27 15:55:52
- * @LastEditTime: 2022-01-27 16:14:41
+ * @LastEditTime: 2022-01-27 16:54:51
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \api\src\controller\user.controller.js
  */
+const { createUser } = require('../service/user.service');
 
 // 放置路由相关接口地址的 处理函数
 class UserController {
     async register(ctx, next) {
-        ctx.body = '用户注册成功'
+        //1. 获取数据 . 使用前 需要 使用 koa-body 的中间件 对 请求体的解析 （ json格式 文本的设置） 
+        // console.log(ctx.request.body)
+        const { user_name, password } = ctx.request.body
+
+        // //2. 操作数据库
+        const res = await createUser(user_name, password)
+        console.log(res);
+
+        // //3. 返回结果
+        ctx.body = ctx.request.body
     }
 
     async login(ctx, next) {
