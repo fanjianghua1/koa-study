942fc5dc467f5d7d4595baec74a7dbe02bdb2924
fjh <1834879843@qq.com>
2022-01-27 15:23:57 +0800

    3-项目的基本优化

---
+++ src/config/config.default.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-27 15:17:38
+ * @LastEditTime: 2022-01-27 15:18:41
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \api\src\config\config.default.js
+ */
+
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+// console.log(process.env.APP_PORT);
+
+module.exports = process.env
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-27 14:03:15
- * @LastEditTime: 2022-01-27 14:19:18
+ * @LastEditTime: 2022-01-27 15:22:45
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \api\src\main.js
@@ -9,13 +9,16 @@
 // 入口文件
 const Koa = require('koa');
 
+// 读取根目录中.env 文件 将配置写入process.env中 ( 通过dotenv 包 )
+const { APP_PORT } = require('./config/config.default');
+
 const app = new Koa()
 
 // ctx 记录 所有http的上下文 context
 app.use((ctx, next) => {
-    ctx.body = 'hello api'
+    ctx.body = 'hello world'
 })
 
-app.listen(3000, () => {
-    console.log('server is running on http://localhost:3000');
+app.listen(APP_PORT, () => {
+    console.log(`server is running on http://localhost:${APP_PORT}`);
 })
